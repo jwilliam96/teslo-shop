e8a832e28e0a082ee62cba0f0b2dd58075fd24d5
jwilliam96 <jwsa.1996@gmail.com>
2024-07-12 12:38:47 -0500

    image

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -2,10 +2,12 @@ export const revalidate = 60; // 60 segundos
 
 import { Pagination, ProductGrid, Title } from '@/components';
 import { getPaginatedProductsWithImages } from '@/actions';
+import imageDesktop from "../../../public/imgs/desktop.webp"
+import imageMobile from "../../../public/imgs/mobile.webp"
 import { compraFont } from '@/config/fonts';
 import { redirect } from 'next/navigation';
 import Link from 'next/link';
-
+import Image from 'next/image';
 interface Props {
   searchParams: {
     page?: string;
@@ -27,12 +29,15 @@ export default async function Home({ searchParams }: Props) {
     <div className='flex-col'>
 
       {/* HEADER  IMAGE DESKTOP*/}
-      <div className='hidden  h-[550px] 3xl:h-[700px] bg-fondo-tesla bg-no-repeat bg-cover bg-center md:flex justify-center items-center'>
-        <Link href="#tienda" className={`text-white text-xl underline font-serif mt-24 hover:text-naranja cursor-pointer`} >COMPRA AHORA</Link>
+      <div className='hidden  h-[550px] 3xl:h-[700px] md:flex justify-center items-center relative'>
+        <Image className='object-cover absolute inset-0 h-full w-full' src={imageDesktop} alt='un hombre y una mujer' priority />
+        <Link href="#tienda" className={`text-white text-xl underline font-serif mt-24 hover:text-naranja cursor-pointer relative`} >COMPRA AHORA</Link>
       </div>
 
       {/* IMAGE MOBILE  */}
-      <div className='md:hidden  h-[500px] 3xl:h-[700px] bg-fondo-mobile bg-no-repeat bg-cover bg-center flex justify-center items-center' />
+      <div className='md:hidden  h-[500px] 3xl:h-[700px] flex justify-center items-center relative'>
+        <Image className='object-cover' src={imageMobile} alt='un hombre y una mujer' fill priority />
+      </div>
 
 
       <p className={`text-center bg-zinc-400 py-2 ${compraFont.className}`}>ENVÍOS GRATIS A TODO EL MUNDO</p>
